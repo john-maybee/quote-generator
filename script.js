4f60adbd2c4896fb6feca6393ef94f5ab05252f4
John Maybee <john.d.maybee@gmail.com>
2023-12-05 15:24:18 -0500

    wrote js to trigger an async function getQuotes to get the json resonse from an api url that is fetched

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('testing');
+let apiQuotes = [];
+
+// Get Quotes From API
+async function getQuotes() {
+    const apiURL = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    try {
+        const response = await fetch(apiURL);
+        apiQuotes = await response.json();
+        console.log(apiQuotes);
+    } catch (error) {
+        // Catch Error Here
+    }
+}
+
+// On Load
+getQuotes();
