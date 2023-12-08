84351ea83d59a46621894be5fd70c0123df56c55
John Maybee <john.d.maybee@gmail.com>
2023-12-08 15:02:54 -0500

    added a catch to the getQuotes function. App now in complete working form

--- script.js
+++ script.js
@@ -3,25 +3,23 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
-const loader = document.getElementById('loader'); // loader is the spinning circle
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
-// Show Loading
-function loading() {
+function showLoadingSpinner() {
     loader.hidden = false;
     quoteContainer.hidden = true;
 }
 
-// Hide Loading
-function complete() {
+function removeLoadingSpinner() {
     quoteContainer.hidden = false;
     loader.hidden = true;
 }
 
 // Show New Quote
 function newQuote() {
-    loading();
+    showLoadingSpinner();
     //Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // Check if Author field is blank and replace it with 'Unknown'
@@ -38,12 +36,12 @@ function newQuote() {
     }
     // Set Quote, Hide Loader
     quoteText.textContent = quote.text;
-    complete();
+    removeLoadingSpinner();
 }
 
 // Get Quotes From API (this entire section could be deleted if quotes were locally stored in another file)
 async function getQuotes() {
-    loading();
+    showLoadingSpinner();
     const apiURL = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
         const response = await fetch(apiURL);
@@ -51,6 +49,7 @@ async function getQuotes() {
         newQuote();
     } catch (error) {
         // Catch Error Here
+        console.log('whoops, no quote', error);
     }
 }
 
